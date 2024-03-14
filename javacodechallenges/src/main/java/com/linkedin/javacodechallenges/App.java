4b1f4a457d645f9b7c98bbf1602010bba9efa265
Dinura Jayatilleke <60333058+Dinura-Jayatilleke@users.noreply.github.com>
2024-03-14 17:49:39 +0000

    challenge 1

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -1,13 +1,22 @@
 package com.linkedin.javacodechallenges;
+import java.util.Scanner;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println("Enter a number:");
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        System.out.println("is "+ n +"even? " +isEven(n));
     }
+
+    public static boolean isEven(int n){
+        if(n%2==0){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    //return (n%2 == 0);
 }
